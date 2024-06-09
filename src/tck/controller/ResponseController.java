53049dacb1f4eec34a3cd086a955241d4b1d2c3c
Messbah.A <Messbah.A@Gmail.Com>
2024-06-09 18:15:54 +0330

    controller updated

--- src/tck/controller/ResponseController.java
+++ src/tck/controller/ResponseController.java
@@ -1,202 +1,104 @@
 package tck.controller;
 
-import com.google.gson.Gson;
-import com.sun.xml.internal.ws.client.ResponseContextReceiver;
-import jdk.internal.dynalink.linker.LinkerServices;
 import tck.model.bl.ResponseBl;
-import tck.model.bl.TicketBl;
-import tck.model.da.TicketDa;
 import tck.model.entity.Person;
 import tck.model.entity.Response;
 import tck.model.entity.Ticket;
-import tck.model.entity.enums.Group;
 import tck.model.entity.enums.Status;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
-@Path("/Response")
 public class ResponseController {
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getResponse() {
-        Response response = Response
-                .builder()
-                .id(1)
-                .person(Person.builder().id(getInt("PERSON_ID")).build())             //TODO
-                .ticket(Ticket.builder().id(getInt("TICKET_ID")).build())             //TODO
-                .answer("we sent your answer")
-                .dateTime(LocalDateTime.now())
-                .status(Status.answer)
-                .build();
-        return Response.status(201).entity(response).build();             //TODO
-
+    public static void save(Ticket ticket, Person person, String answer, Status status, LocalDateTime dateTime) {
+        try {
+            Response response = new Response();
+            response.setPerson(person).setTicket(ticket).setAnswer(answer).setStatus(status).setLocalDateTime(dateTime);            //TODO
+            ResponseBl.getResponseBl().save(response);
+            System.out.println("response saved.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    @POST
-    public Response save(
-            @QueryParam("id") int id,
-            @QueryParam("ticketId") int ticketId,           //TODO : int  ticketId ?
-            @QueryParam("personId") int personId,
-            @QueryParam("answer") String answer,
-            @QueryParam("status") Status status,
-            @QueryParam("dateTime") LocalDateTime dateTime) throws Exception {         //TODO
-        return Response
-                .builder()
-                .id(1)
-                .person(Person.builder().id(getInt("PERSON_ID")).build())             //TODO
-                .ticket(Ticket.builder().id(getInt("TICKET_ID")).build())             //TODO
-                .answer("we sent your answer")
-                .dateTime(LocalDateTime.now())
-                .status(Status.answer)
-                .build();
-        ResponseBl.getResponseBl().save(getResponse());             // TODO :  is it right??
+    public static void edit(int id, Person person, Ticket ticket, String answer, Status status, LocalDateTime dateTime) {
+        try {
+            Response response = Response.builder().id(id).status(Status.valueOf("status")).person(person.setId(id)).ticket(ticket.setId(id)).answer(answer).dateTime(LocalDateTime.now()).build();            //TODO FOR DATE   &   SetId!
+            // TODO for SET??
+            ResponseBl.getResponseBl().edit(response);
+            System.out.println("response updated!.");
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+        }
     }
 
-    @PUT
-    public Response edit(
-            @QueryParam("id") int id,
-            @QueryParam("ticketId") int ticketId,           //TODO : int  ticketId ?
-            @QueryParam("personId") int personId,
-            @QueryParam("answer") String answer,
-            @QueryParam("status") Status status,
-            @QueryParam("dateTime") LocalDateTime dateTime) throws Exception {         //TODO
-        return Response
-                .builder()
-                .id(1)
-                .person(Person.builder().id(getInt("PERSON_ID")).build())             //TODO
-                .ticket(Ticket.builder().id(getInt("TICKET_ID")).build())             //TODO
-                .answer("we sent your answer")
-                .dateTime(LocalDateTime.now())
-                .status(Status.answer)
-                .build();
-        ResponseBl.getResponseBl().edit(getResponse());             // TODO :  is it right??
+    public static void findAll(int id, Ticket ticket, Person person, String answer, Status status, LocalDateTime dateTime) {
+        try {
+            Response response = Response.builder().id(id).status(Status.valueOf("status")).person(person.setId(id)).ticket(ticket.setId(id)).answer(answer).dateTime(LocalDateTime.now()).build();
+            List<Response> responseList = ResponseBl.getResponseBl().findAll();           //TODO
+            System.out.println("info:response found");
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+        }
     }
 
-    @GET
-    public String find() {          // TODO : EVERYWHERE??
-        return "Find";
+    public static void findById(int id, Ticket ticket, Person person, String answer, Status status, LocalDateTime dateTime) {
+        try {
+            Response response = Response.builder().id(id).status(Status.valueOf("status")).person(person.setId(id)).ticket(ticket.setId(id)).answer(answer).dateTime(LocalDateTime.now()).build();
+            ResponseBl.getResponseBl().findById(id);
+            System.out.println("info:response found by id");
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+        }
     }
 
-    @GET
-    @Path("/all")
-    public String findAll() throws Exception {
-        List<Response> responseList = new ArrayList<>();
-        ResponseBl.getResponseBl().findAll();
-        responseList.add(response);                   //TODO
-        Gson gson = new Gson();
-        return gson.toJson(responseList);
+    public static void findByPersonId(int id, Ticket ticket, Person person, String answer, Status status, LocalDateTime dateTime) {
+        try {
+            Response response = Response.builder().id(id).status(Status.valueOf("status")).person(person.setId(id)).ticket(ticket.setId(id)).answer(answer).dateTime(LocalDateTime.now()).build();
+            ResponseBl.getResponseBl().findByPersonId(id);
+            System.out.println("info:response found by person id");
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+        }
     }
 
-    @GET
-    @Path("/all{id}")
-    public String findById(@PathParam("id") int id) throws Exception {
-        Response response = Response
-                .builder()
-                .id(1)
-                .person(getResponse().getPerson().setId(id))            //TODO
-                .ticket(getResponse().getTicket().setId(id))
-                .answer("we sent your answer")
-                .status(Status.answer)
-                .dateTime(LocalDateTime.now())
-                .build();
-        List<Response> responseList = new ArrayList<>();     //TODO
-        ResponseBl.getResponseBl().findById(response.getId());      // TODO ; TRUE ??
-        Gson gson = new Gson();
-        return gson.toJson(responseList.get(id));
+    public static void findByTicketId(int id, Ticket ticket, Person person, String answer, Status status, LocalDateTime dateTime) {
+        try {
+            Response response = Response.builder().id(id).status(Status.valueOf("status")).person(person.setId(id)).ticket(ticket.setId(id)).answer(answer).dateTime(LocalDateTime.now()).build();
+            ResponseBl.getResponseBl().findByTicketId(id);
+            System.out.println("info:response found by ticket id");
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+        }
     }
 
-    @Path("/all/{personId}")            //TODO  :   personId?
-    public String findByPersonId(@PathParam("personId") int id) throws Exception {
-        Response response = Response
-                .builder()
-                .id(1)
-                .person(getResponse().getPerson().setId(id))            //TODO
-                .ticket(getResponse().getTicket().setId(id))              // TODO
-                .answer("we sent your answer")
-                .status(Status.answer)
-                .dateTime(LocalDateTime.now())
-                .build();
-        List<Response> responseList = new ArrayList<>();
-        ResponseBl.getResponseBl().findByPersonId(id);
-        Gson gson = new Gson();
-        return gson.toJson(responseList);
+    public static void findByAnswer(int id, Ticket ticket, Person person, String answer, Status status, LocalDateTime dateTime) {
+        try {
+            Response response = Response.builder().id(id).status(Status.valueOf("status")).person(person.setId(id)).ticket(ticket.setId(id)).answer(answer).dateTime(LocalDateTime.now()).build();
+            ResponseBl.getResponseBl().findByAnswer(answer);
+            System.out.println("info:response found by answer");
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+        }
     }
 
-    @Path("/all/{ticketId}")            //TODO  :   personId?
-    public String findByTicketId(@PathParam("ticketId") int id) throws Exception {
-        Response response = Response
-                .builder()
-                .id(1)
-                .person(getResponse().getPerson().setId(id))            //TODO
-                .ticket(Ticket.builder().id(id).build())                        // TODO
-                .answer("we sent your answer")
-                .status(Status.answer)
-                .dateTime(LocalDateTime.now())
-                .build();
-        List<Response> responseList = new ArrayList<>();
-        ResponseBl.getResponseBl().findByTicketId(id);
-        Gson gson = new Gson();
-        return gson.toJson(responseList);
+    public static void findByDateTime(int id, Ticket ticket, Person person, String answer, Status status, LocalDateTime dateTime) {
+        try {
+            Response response = Response.builder().id(id).status(Status.valueOf("status")).person(person.setId(id)).ticket(ticket.setId(id)).answer(answer).dateTime(LocalDateTime.now()).build();
+            ResponseBl.getResponseBl().findByDateTime(dateTime);        // TODO : CHECK!!!
+            System.out.println("info:response found by date time");
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+        }
     }
 
-    @Path("/all/{status}")
-    public String findByStatus(@PathParam("status") Status status) throws Exception {
-        Response response = Response
-                .builder()
-                .id(1)
-                .person(getResponse().getPerson().setId(id))            //TODO
-                .ticket(Ticket.builder().id().build())                        // TODO
-                .answer("we sent your answer")
-                .status(Status.answer)
-                .dateTime(LocalDateTime.now())
-                .build();
-        List<Response> responseList = new ArrayList<>();
-        ResponseBl.getResponseBl().findByStatus(status);
-        responseList.add(response);
-        Gson gson = new Gson();
-        return gson.toJson(responseList.get(Status.valueOf()));         //TODO
-    }
-
-    @Path("/all/{answer}")
-    public String findByAnswer(@PathParam("answer") String answer) throws Exception {
-        Response response = Response
-                .builder()
-                .id(1)
-                .person(getResponse().getPerson().setId(id))            //TODO
-                .ticket(Ticket.builder().id().build())                        // TODO
-                .answer("we sent your answer")
-                .status(Status.answer)
-                .dateTime(LocalDateTime.now())
-                .build();
-        List<Response> responseList = new ArrayList<>();
-        ResponseBl.getResponseBl().findByAnswer(answer);
-        responseList.add(response);
-        Gson gson = new Gson();
-        return gson.toJson(responseList);           //TODO ??
-    }
-
-    @Path("/all/{dateTime}")
-    public String findByDateTime(@PathParam("dateTime") LocalDateTime dateTime) throws Exception {  //TODO
-        Response response = Response
-                .builder()
-                .id(1)
-                .person(getResponse().getPerson().setId(id))            //TODO
-                .ticket(Ticket.builder().id().build())                        // TODO
-                .answer("we sent your answer")
-                .status(Status.answer)
-                .dateTime(LocalDateTime.now())
-                .build();
-        List<Response> responseList = new ArrayList<>();
-        ResponseBl.getResponseBl().findByDateTime(dateTime);            //TODO
-        responseList.add(response);
-        Gson gson = new Gson();
-        return gson.toJson(responseList);                       //TODO
+    public static void findByStatus(int id, Ticket ticket, Person person, String answer, Status status, LocalDateTime dateTime) {
+        try {
+            Response response = Response.builder().id(id).status(Status.valueOf("status")).person(person.setId(id)).ticket(ticket.setId(id)).answer(answer).dateTime(LocalDateTime.now()).build();
+            ResponseBl.getResponseBl().findByStatus(status);
+            System.out.println("info:response found by status");
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+        }
     }
 }
-
-
-
